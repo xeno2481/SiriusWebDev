1153f83dfe6b4a76f22ade01aad777ca88c5a156
Joshua Sadler <jrsadler18@gmail.com>
2025-05-14 23:21:13 -0600

    Website Setupu

---
+++ src/main/java/com/joshuasadler/siriuswebdev/SiriusWebDevApplication.java
@@ -0,0 +1,11 @@
+package com.joshuasadler.siriuswebdev;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+
+@SpringBootApplication
+public class SiriusWebDevApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SiriusWebDevApplication.class, args);
+    }
+}
